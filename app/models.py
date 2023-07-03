936d0c385b5e5e063a8d706142df7b7e3c2c24f7
Kennedy <kiplangatkennedy019@gmail.com>
2023-07-03 12:02:55 +0300

    added table columns

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+import datetime
+from attr import validate
 
 db = SQLAlchemy()
 
@@ -6,5 +8,9 @@ class Episode(db.Model):
     __tablename__ = 'Episode'
 
     id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(10), nullable=False)
+    number = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
 # add any models you may need. 
